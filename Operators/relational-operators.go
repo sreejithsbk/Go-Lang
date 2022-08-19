1aabcd5a7e38d33cf1a30d51f3a107a8ce5a46b7
sreejithsbk <50789873+sreejithsbk@users.noreply.github.com>
2022-08-19 13:13:33 +0530

    relational operator

---
+++ Operators/relational-operators.go
@@ -0,0 +1,17 @@
+// == // It checks if the values of two operands are equal or not; if yes, the condition becomes true.
+// (A == B) is not true.
+
+// !=  It checks if the values of two operands are equal or not; if the values are not equal, then the condition becomes true.
+// (A != B) is true.
+
+// >  It checks if the value of left operand is greater than the value of right operand; if yes, the condition becomes true.
+// (A > B) is not true.
+
+// < It checks if the value of left operand is less than the value of the right operand; if yes, the condition becomes true.
+// (A < B) is true
+
+// >= It checks if the value of the left operand is greater than or equal to the value of the right operand; if yes, the condition becomes true.
+// (A >= B) is not true.
+
+// <= It checks if the value of left operand is less than or equal to the value of right operand; if yes, the condition becomes true.
+// (A <= B) is true.

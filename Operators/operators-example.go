b0ba925229dbf33fe2963d0d2a0e0d37889726a2
sreejithsbk <50789873+sreejithsbk@users.noreply.github.com>
2022-08-19 13:10:48 +0530

    operators example

---
+++ Operators/operators-example.go
@@ -0,0 +1,15 @@
+// Arithmetic Operators
+
+var total = 3 + 3
+
+// Arithmetic Assignments
+
+a = 1     a := 1
+a += 3    a = a + 3  // both the ways are the same 
+
+//increment and decrement . These are very common in loops
+
+i++
+i--
+
+// Arithmetic operators always return a number.

e224d912dfc47c2d8109c88aa015f215ad30bf5e
sreejithsbk <50789873+sreejithsbk@users.noreply.github.com>
2022-08-19 13:15:41 +0530

    initialization

---
+++ If-else/statement-initialization.go
@@ -0,0 +1,14 @@
+if i := 5; i < 10 {
+	// do something with i
+} else {
+	//do something else with i
+}
+
+
+if rank := getUserRank(); rank == "admin" {
+	//do admin stuff here
+} else if rank == "manager"
+    // do manager stuff here
+} else {
+	// display error message
+}

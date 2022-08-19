4c87d1905125761f5c663c998e065b6f42f63ac1
sreejithsbk <50789873+sreejithsbk@users.noreply.github.com>
2022-08-19 13:10:14 +0530

    logical operators

---
+++ Operators/logical-operators.go
@@ -0,0 +1,8 @@
+ // && Called Logical AND operator. If both the operands are non-zero, then condition becomes true.
+ (A && B) is false.
+
+ // || Called Logical OR Operator. If any of the two operands is non-zero, then condition becomes true.
+ (A || B) is true.
+
+ // ! Called Logical NOT Operator. Use to reverses the logical state of its operand. If a condition is true then Logical NOT operator will make false.
+ !(A && B) is true.
